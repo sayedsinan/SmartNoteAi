a9b5c092cfdbe5e4dd99139f44b033433918c864
Sooraj-kj <ajilakhil88@gmail.com>
2026-06-03 11:40:28 +0530

    issue fixed

--- services/noteService.js
+++ services/noteService.js
@@ -1,52 +1,127 @@
-const db =require('../database/db');
+const db = require('../database/db');
+
 const noteService = {};
 
-noteService.createNote = (title, content, callback) => {
-    const query = 'INSERT INTO notes (title, content) VALUES (?, ?)';
-    db.query(query, [title, content], (err, result) => {
-        if (err) {
-            return callback(err);
+// Create Note
+noteService.createNote = (
+    title,
+    original_notes,
+    generated_content,
+    difficulty_level,
+    callback
+) => {
+
+    const query = `
+        INSERT INTO notes
+        (title, original_notes, generated_content, difficulty_level)
+        VALUES (?, ?, ?, ?)
+    `;
+
+    db.query(
+        query,
+        [
+            title,
+            original_notes,
+            JSON.stringify(generated_content),
+            difficulty_level
+        ],
+        (err, result) => {
+            if (err) {
+                return callback(err);
+            }
+
+            callback(null, result.insertId);
         }
-        callback(null, result.insertId);
-    });
+    );
 };
 
+// Get All Notes
 noteService.getAllNotes = (callback) => {
-    const query = 'SELECT * FROM notes';
-    db.query(query, (err, results) => {
-        if (err) {
-            return callback(err);
+
+    db.query(
+        'SELECT * FROM notes ORDER BY created_at DESC',
+        (err, results) => {
+
+            if (err) {
+                return callback(err);
+            }
+
+            callback(null, results);
         }
-        callback(null, results);
-    });
+    );
 };
 
+// Get Note By Id
 noteService.getNoteById = (id, callback) => {
-    const query = 'SELECT * FROM notes WHERE id = ?';
-    db.query(query, [id], (err, results) => {
-        if (err) {
-            return callback(err);
-        }  
-        callback(null, results[0]);
-    });
+
+    db.query(
+        'SELECT * FROM notes WHERE id = ?',
+        [id],
+        (err, results) => {
+
+            if (err) {
+                return callback(err);
+            }
+
+            callback(null, results[0]);
+        }
+    );
 };
 
-noteService.updateNote = (id, title, content, callback) => {       
+// Update Note
+noteService.updateNote = (
+    id,
+    title,
+    original_notes,
+    generated_content,
+    difficulty_level,
+    callback
+) => {
 
-    const query = 'UPDATE notes SET title = ?, content = ? WHERE id = ?';
-    db.query(query, [title, content, id], (err, result) => {
-        if (err) {
-            return callback(err);
+    const query = `
+        UPDATE notes
+        SET title = ?,
+            original_notes = ?,
+            generated_content = ?,
+            difficulty_level = ?
+        WHERE id = ?
+    `;
+
+    db.query(
+        query,
+        [
+            title,
+            original_notes,
+            JSON.stringify(generated_content),
+            difficulty_level,
+            id
+        ],
+        (err, result) => {
+
+            if (err) {
+                return callback(err);
+            }
+
+            callback(null, result.affectedRows);
         }
-        callback(null, result.affectedRows);
-    });
- }
- noteService.deleteNote = (id, callback) => {
-    const query = 'DELETE FROM notes WHERE id = ?';
-    db.query(query, [id], (err, result) => {
-        if (err) {
-            return callback(err);
-        }
-        callback(null, result.affectedRows);
-    });
+    );
 };
+
+// Delete Note
+noteService.deleteNote = (id, callback) => {
+
+    db.query(
+        'DELETE FROM notes WHERE id = ?',
+        [id],
+        (err, result) => {
+
+            if (err) {
+                return callback(err);
+            }
+
+            callback(null, result.affectedRows);
+        }
+    );
+};
+
+module.exports = noteService;
